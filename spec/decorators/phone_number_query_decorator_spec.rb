821dab694c89025993239038061cc4d02fa2f0a4
David Wilkie <dwilkie@gmail.com>
2015-10-02 08:43:31 +0700

    Don't make assertions on layout of results

--- spec/decorators/phone_number_query_decorator_spec.rb
+++ spec/decorators/phone_number_query_decorator_spec.rb
@@ -11,11 +11,6 @@ describe PhoneNumberQueryDecorator do
 
   subject { described_class.new(phone_number_query) }
 
-  def assert_description_list_item!(html)
-    expect(html).to have_css("dt")
-    expect(html).to have_css("dd")
-  end
-
   describe "#phone_number" do
     let(:result) { subject.phone_number }
 
@@ -24,7 +19,6 @@ describe PhoneNumberQueryDecorator do
     end
 
     it { expect(result).to have_link("+855 12 222 333") }
-    it { assert_description_list_item!(result) }
   end
 
   describe "#phone_number_type" do
@@ -41,13 +35,11 @@ describe PhoneNumberQueryDecorator do
     context "for mobile numbers" do
       let(:phone_number_type) { "mobile" }
       it { expect(result).to have_content("Mobile") }
-      it { assert_description_list_item!(result) }
     end
 
     context "for landline numbers" do
       let(:phone_number_type) { "landline" }
       it { expect(result).to have_content("Landline") }
-      it { assert_description_list_item!(result) }
     end
   end
 
@@ -73,7 +65,6 @@ describe PhoneNumberQueryDecorator do
     context "operator has a name" do
       let(:operator_name) { "Mobitel" }
       it { expect(result).to have_content("Mobitel") }
-      it { assert_description_list_item!(result) }
 
       context "but different branding" do
         let(:operator_branding) { "Cellcard" }
@@ -85,19 +76,16 @@ describe PhoneNumberQueryDecorator do
     context "operator has a customer service phone" do
       let(:operator_customer_service_phone) { "85512202101" }
       it { expect(result).to have_link("operator_customer_service_phone", :href => "tel:+855 12 202 101") }
-      it { assert_description_list_item!(result) }
     end
 
     context "operator has a facebook page" do
       let(:operator_facebook) { "https://facebook.com/cellcard" }
       it { expect(result).to have_link("operator_facebook", :href => operator_facebook) }
-      it { assert_description_list_item!(result) }
     end
 
     context "operator has a website" do
       let(:operator_website) { "https://mobitel.com" }
       it { expect(result).to have_link("operator_website", :href => operator_website) }
-      it { assert_description_list_item!(result) }
     end
   end
 
@@ -121,25 +109,21 @@ describe PhoneNumberQueryDecorator do
     context "operator CEO has a name" do
       let(:operator_ceo_name) { "Ian Watson" }
       it { expect(result).to have_content("Ian Watson") }
-      it { assert_description_list_item!(result) }
     end
 
     context "operator CEO has a phone" do
       let(:operator_ceo_phone) { "85512202101" }
       it { expect(result).to have_link("operator_ceo_phone", :href => "tel:+855 12 202 101") }
-      it { assert_description_list_item!(result) }
     end
 
     context "operator CEO has an email" do
       let(:operator_ceo_email) { "ian.watson@cellcard.com.kh" }
       it { expect(result).to have_link("operator_ceo_email", :href => "mailto:ian.watson@cellcard.com.kh") }
-      it { assert_description_list_item!(result) }
     end
 
     context "operator CEO has a linked in profile" do
       let(:operator_ceo_linked_in) { "https://linkedin.com/ian_watson" }
       it { expect(result).to have_link("operator_ceo_linked_in", :href => "https://linkedin.com/ian_watson") }
-      it { assert_description_list_item!(result) }
     end
   end
 
@@ -159,7 +143,6 @@ describe PhoneNumberQueryDecorator do
     context "location has a country" do
       let(:location_country_name) { "Cambodia" }
       it { expect(result).to have_link("Cambodia", :href => "http://maps.google.com?q=Cambodia") }
-      it { assert_description_list_item!(result) }
 
       context "and an area" do
         let(:location_area) { "Phnom Penh" }
