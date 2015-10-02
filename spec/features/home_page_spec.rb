602fc0b247a18ec77394f7c159bd5efec267ac4a
David Wilkie <dwilkie@gmail.com>
2015-10-02 09:30:40 +0700

    Add integration test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,18 +1,29 @@
 require "rails_helper"
 
-describe "Home Page" do
+describe "'/'" do
   before do
-    visit root_path
+    visit(root_path)
   end
 
-  it { expect(current_path).to eq(root_path) }
+  describe "Torasup Search" do
+    let(:error_message) { "is an invalid number" }
+    let(:result_selector) { "#result" }
 
-  describe "searching" do
     before do
-      fill_in("search_box", :with => search_term)
-      click_button("Search")
+      fill_in("phone_number_query_phone_number", :with => search_term)
+      click_button("Torasup Search")
     end
 
-    it { expect() }
+    context "valid number" do
+      let(:search_term) { "012 345 568" }
+      it { expect(page).to have_css(result_selector) }
+      it { expect(page).to have_no_content(error_message) }
+    end
+
+    context "invalid number" do
+      let(:search_term) { "012 345 5689" }
+      it { expect(page).to have_no_css(result_selector) }
+      it { expect(page).to have_content(error_message) }
+    end
   end
 end
